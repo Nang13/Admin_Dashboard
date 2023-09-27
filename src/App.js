229fa72751e84a9424d636f022e1b56186fd0cd2
Nắng <99975060+Nang13@users.noreply.github.com>
2023-09-27 14:27:44 +0700

    [Sidebar]

--- src/App.js
+++ src/App.js
@@ -20,10 +20,10 @@ const app = () => {
           </div>
           {activeMenu ? (
             <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-              Sidebar
+              <Sidebar/>
             </div>
           ) : (<div className='w-0 dark:bg-secondary-dark-bg'>
-            Sidebar w-0
+            <Sidebar/>
           </div>)}
           <div className={`dark:bg-main-bg bg-main-bg min-h-screen  w-full ${activeMenu ? 'md:ml-72' : 'flex-2'} `}>
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
@@ -63,4 +63,4 @@ const app = () => {
   )
 }
 
-export default app
+export default app  
