e3afba5f94fd96225fba9b0b4b4007b6b7c232be
Nắng <99975060+Nang13@users.noreply.github.com>
2023-10-08 22:17:44 +0700

    Cuu Man

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Login from './components/Login'
 import { ContextProvider } from './contexts/ContextProvider'
 
+// ReactDOM.render(
+//     <ContextProvider>
+//         <Login />
+//     </ContextProvider>
+//     , document.getElementById('root'))
 ReactDOM.render(
-    <ContextProvider>
-        <App />
-    </ContextProvider>
-    , document.getElementById('root'))
+    <Login />, document.getElementById('root')
+)
